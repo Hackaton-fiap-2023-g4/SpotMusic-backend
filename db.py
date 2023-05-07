04ad7c5af3c6c906fa9c39689212c537461cbe39
rsfernandes <64504126+rsfernandes@users.noreply.github.com>
2023-05-07 00:56:51 -0300

    Update db.py

--- db.py
+++ db.py
@@ -5,8 +5,11 @@ from flask import jsonify
 db_user = os.environ.get('CLOUD_SQL_USERNAME')
 db_password = os.environ.get('CLOUD_SQL_PASSWORD')
 db_name = os.environ.get('CLOUD_SQL_DATABASE_NAME')
-db_local_host = os.environ.get('DB_LOCAL_HOST')
+#db_local_host = os.environ.get('DB_LOCAL_HOST')
+db_local_host = 10.90.160.3
 db_connection_name = os.environ.get('CLOUD_SQL_CONNECTION_NAME')
+
+
 
 def open_connection():
     try:
